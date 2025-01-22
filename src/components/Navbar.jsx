3d48f744ffdb3477ed0658a47af25e67979f46b1
--global <--global>
2025-01-22 17:06:03 +0000

    Modificación de estilos del navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,31 +5,33 @@ import LanguageSwitcher from "./LanguageSwitcher";
 import navbarOptions from "../constants/navbarOptions";
 import "../styles/navbar.css";
 
+import { FiUser } from "react-icons/fi";
+
 const Navbar = () => {
   const { t } = useTranslation();
 
   return (
     <nav className="navbar">
       <div className="navbar__logo">
-        <Link to="/">
-          <img src="/path-to-logo.png" alt="Logo" />
-        </Link>
+        <Link to="/">Logo</Link>
       </div>
 
       <div className="navbar__menu">
-        <ul>
+        <ul className="navbar__menu-list-item">
           {navbarOptions.map((option, index) => (
             <li key={index} className="navbar__menu-item">
               <Link to={option.url}>{t(option.nameKey)}</Link>
             </li>
           ))}
         </ul>
+
+        <LanguageSwitcher />
       </div>
 
-      <LanguageSwitcher />
-
       <div className="navbar__right-section">
-        <button className="navbar__login-button">{t("buttons.login")}</button>
+        <button className="navbar__login-button">
+          <FiUser />
+        </button>
       </div>
     </nav>
   );
