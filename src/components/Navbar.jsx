fca5a7d112e4100f444e99e8594c26606e9f0619
--global <--global>
2025-02-13 12:38:41 +0000

    Añadiendo funcionalidad de menú hamburguesa al navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "./LanguageSwitcher";
 import navbarOptions from "../constants/navbarOptions";
 import "../styles/navbar.css";
 
-import { FiUser } from "react-icons/fi";
+import { FiUser, FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
   const { t } = useTranslation();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="navbar">
@@ -16,13 +17,39 @@ const Navbar = () => {
         <Link to="/">Logo</Link>
       </div>
 
-      <div className="navbar__menu">
+      <button
+        className="navbar__hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <FiMenu />
+      </button>
+
+      <div className={`navbar__menu ${menuOpen ? "navbar__menu--open" : ""}`}>
+        {menuOpen ? (
+          <button
+            className="navbar__menu-close"
+            onClick={() => setMenuOpen(false)}
+          >
+            <FiX />
+          </button>
+        ) : (
+          ""
+        )}
+
         <ul className="navbar__menu-list-item">
           {navbarOptions.map((option, index) => (
             <li key={index} className="navbar__menu-item">
-              <Link to={option.url}>{t(option.nameKey)}</Link>
+              <Link to={option.url} onClick={() => setMenuOpen(false)}>
+                {t(option.nameKey)}
+              </Link>
             </li>
           ))}
+
+          <li className="navbar__menu-item navbar__menu-item--mobile">
+            <Link to="/login" onClick={() => setMenuOpen(false)}>
+              <FiUser /> {t("login")}
+            </Link>
+          </li>
         </ul>
 
         <LanguageSwitcher />
