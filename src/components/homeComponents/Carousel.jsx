890317e898a75078e8b4007f7256d050d5527f51
--global <--global>
2025-02-15 16:33:55 +0000

    Añadiendo texto a la cabecera de home

--- src/components/homeComponents/Carousel.jsx
+++ src/components/homeComponents/Carousel.jsx
@@ -1,18 +1,20 @@
+import { useTranslation } from "react-i18next";
+import { carouselImages } from "../../constants/carouselImages";
 import "../../styles/carousel.css";
 
-const images = [
-  "/assets/carouselImage1.jpg",
-  "/assets/carouselImage2.jpg",
-  "/assets/carouselImage3.jpg",
-  "/assets/carouselImage4.jpg",
-  "/assets/carouselImage5.jpg",
-];
-
 export default function Carousel() {
+  const { t } = useTranslation();
+
   return (
     <div className="carousel">
+      <div className="carousel-content--wrapper">
+        <div className="carousel-content">
+          <p>{t("home.header")}</p>
+        </div>
+      </div>
+
       <div className="carousel-track">
-        {[...images, ...images].map((img, i) => (
+        {[...carouselImages, ...carouselImages].map((img, i) => (
           <img key={i} src={img} alt={`Imagen carrusel ${i + 1}`} />
         ))}
       </div>
---
+++ src/constants/carouselImages.js
@@ -0,0 +1,7 @@
+export const carouselImages = [
+  "/assets/carouselImage1.jpg",
+  "/assets/carouselImage2.jpg",
+  "/assets/carouselImage3.jpg",
+  "/assets/carouselImage4.jpg",
+  "/assets/carouselImage5.jpg",
+];
